6bc896dbd9dd2dcc6ba448a2e17890b5a97e0a76
Dmitry Harnitski <dharnitski@gmail.com>
2019-04-16 22:18:49 -0400

    add ts test

---
+++ src/tests/app.component.spec.ts
@@ -0,0 +1,8 @@
+import { AppComponent } from "~/app/app.component";
+
+describe('AppComponent', () => {
+    it('should create the app', () => {
+        const app = new AppComponent();
+        expect(app).toBeTruthy();
+    });
+});
